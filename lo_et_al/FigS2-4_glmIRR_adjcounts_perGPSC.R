177a9fda903d2bcf1f99b6b76f182be9d6dca714
Rebecca Gladstone <rg9@sanger.ac.uk>
2019-01-15 12:05:03 +0000

    Add files via upload

--- lo_et_al/FigS2-4_glmIRR_adjcounts_perGPSC.R
+++ lo_et_al/FigS2-4_glmIRR_adjcounts_perGPSC.R
@@ -1,5 +1,3 @@
-#Significant changes measure with IRR via poisson regression within GPSC for South Africa, USA and Israel for figures S2-4
-
 require(tidyverse)
 
 #input population size per year from https://github.com/rgladstone/GPSCs/blob/master/lo_et_al/pop_years.csv
